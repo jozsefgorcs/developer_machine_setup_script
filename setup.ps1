242165dbc2886f72d1109d1be882949334e425a5
Jozsef Gorcs <34841549+jozsefgorcs@users.noreply.github.com>
2023-03-19 10:58:08 +0100

    Update setup.ps1
    Add node js

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,1 @@
-choco install googlechrome vlc audacity 7zip winrar notepadplusplus.install vscode thunderbird irfanview totalcommander telegram viber firefox bitwarden w10privacy vnc-viewer git.install sourcetree neovim libreoffice-fresh putty paint.net calibre docker-cli python postman linqpad7 -y
+choco install googlechrome vlc audacity 7zip winrar notepadplusplus.install vscode thunderbird irfanview totalcommander telegram viber firefox bitwarden w10privacy vnc-viewer git.install sourcetree neovim libreoffice-fresh putty paint.net calibre docker-cli python postman linqpad7 nodejs-lts -y
